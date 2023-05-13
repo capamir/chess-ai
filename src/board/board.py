97cd207cd00adf318ec0c692df3b84525498290f
amirhossein <79211332+capamir@users.noreply.github.com>
2023-05-13 16:45:27 +0330

    functions description of the board class

--- src/board/board.py
+++ src/board/board.py
@@ -11,11 +11,17 @@ class Board:
 
 
     def _create(self):
+        """
+            creates the squares for the game board
+        """
         for row in range(ROWS):
             for col in range(COLS):
                 self.squares[row][col] = Square(row, col)
 
     def _add_piece(self, color):
+        """
+            create the pieces in thier original position at the beginning of the game
+        """
         row_pawn, row_other = (6, 7) if color == 'white' else (1, 0)
 
         # pawns
@@ -39,3 +45,8 @@ class Board:
 
         # king
         self.squares[row_other][4] = Square(row_other, 4, King(color))
+
+    def calc_moves(self, piece, row, col):
+        """
+            claculate all the possible (valid) moves of a specific piece on the specific position
+        """
